5d7350f4dc822725df2037b7c083a30f6e3e43c5
Gregor Daiss <Gregor.Daiss+git@gmail.com>
2023-07-17 21:10:05 -0500

    Add kokkos cpu variants

--- packages/octotiger/package.py
+++ packages/octotiger/package.py
@@ -3,30 +3,12 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install octotiger
-#
-# You can edit this file again by typing:
-#
-#     spack edit octotiger
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack.package import *
 
 
-class Octotiger(CMakePackage):
-    """FIXME: Put a proper description of your package here."""
+class Octotiger(CMakePackage, CudaPackage, ROCmPackage):
+    """Octo-Tiger is an astrophysics program simulating the evolution of star systems based on the fast multipole method on adaptive Octrees. It was implemented using high-level C++ libraries, specifically HPX and Vc, which allows its use on different hardware platforms."""
 
-    # FIXME: Add a proper url for your package's homepage here.
     homepage = "https://github.com/STEllAR-GROUP/octotiger"
     url = "https://github.com/STEllAR-GROUP/octotiger"
     git = "https://github.com/STEllAR-GROUP/octotiger"
@@ -45,7 +27,23 @@ class Octotiger(CMakePackage):
         'theta_minimum', default='0.34', description='Octotiger minimal allowed theta value',
         values=('0.34', '0.5', '0.16'), multi=False
     )
-
+    variant('kokkos_hpx_kernels', default=False,
+            description='Use HPX execution space for CPU Kokkos kernels (instead of the Serial space)')
+    variant('monopole_host_tasks', default='1',
+            description='Tasks per monopole kernel invocation when using the Kokkos HPX execution space',
+            values=('1', '4', '16'), multi=False)
+    variant('multipole_host_tasks', default='1',
+            description='Tasks per multipole kernel invocation when using the Kokkos HPX execution space',
+            values=('1', '4', '16', '64'), multi=False)
+    variant('hydro_host_tasks', default='1',
+            description='Tasks per hydro kernel invocation when using the Kokkos HPX execution space',
+            multi=False)
+    variant('simd_library', default='KOKKOS',
+            description='Use either kokkos (for kokkos simd types) or std (for std::experimental::simd) for the simd types',
+            values=('KOKKOS', 'STD'), multi=False)
+    variant('simd_extension', default='DISCOVER',
+            description='Enforce specific SIMD extension or autoselect (discover) appropriate one',
+            values=('DISCOVER', 'SCALAR', 'AVX', 'AVX512', 'NEON', 'SVE'), multi=False)
 
     depends_on('cppuddle +hpx_support')
 
@@ -67,18 +65,28 @@ class Octotiger(CMakePackage):
     depends_on('cuda', when='+cuda')
 
     hpx_string = 'hpx@1.8.0: cxxstd=17'
-    depends_on(hpx_string + ' +cuda +async_cuda', when='+cuda') #networking=mpi ?
+    depends_on(hpx_string + ' +cuda +async_cuda ', when='+cuda') #networking=mpi ?
     depends_on(hpx_string + ' -cuda', when='-cuda')
 
-    kokkos_string = 'kokkos +serial +hpx +hpx_async_dispatch +aggressive_vectorization '
-    #depends_on(kokkos_string + ' +cuda +cuda_lambda +wrapper ', when='+kokkos +cuda', patches=['adapt-kokkos-wrapper-for-nix.patch', 'adapt-kokkos-wrapper-for-hpx.patch'])
-    #depends_on(kokkos_string + ' -cuda -cuda_lambda -wrapper',when='+kokkos -cuda', patches=['adapt-kokkos-wrapper-for-nix.patch', 'adapt-kokkos-wrapper-for-hpx.patch'] )
-    depends_on(kokkos_string + ' +cuda +cuda_lambda +wrapper ', when='+kokkos +cuda')
+    kokkos_string = 'kokkos +serial +aggressive_vectorization '
+    for sm_ in CudaPackage.cuda_arch_values:                                                                                                                                                                                                                                
+        depends_on(kokkos_string + ' +cuda +cuda_lambda +wrapper cuda_arch={0}'.format(sm_), when='+kokkos +cuda cuda_arch={0} %gcc'.format(sm_))
+        depends_on(kokkos_string + ' +cuda +cuda_lambda -wrapper cuda_arch={0}'.format(sm_), when='+kokkos +cuda cuda_arch={0} %clang'.format(sm_))
+    depends_on("kokkos@4.1.00: +hpx +hpx_async_dispatch ", when="+kokkos_hpx_kernels")
     depends_on(kokkos_string + ' -cuda -cuda_lambda -wrapper',when='+kokkos -cuda')
-
-    #depends_on('kokkos-nvcc-wrapper', when='+kokkos')
     depends_on('spack.pkg.builtin.kokkos-nvcc-wrapper', when='+kokkos',
               patches=['adapt-kokkos-wrapper-for-nix.patch', 'adapt-kokkos-wrapper-for-hpx.patch'])
+
+    conflicts("+cuda", when="cuda_arch=none")
+    conflicts("+kokkos_hpx_kernels", when="~kokkos")
+    conflicts("simd_library=STD", when="%gcc@:10")
+    conflicts("simd_library=STD", when="%clang")
+
+    #depends_on(kokkos_string + ' +cuda +cuda_lambda +wrapper ', when='+kokkos +cuda', patches=['adapt-kokkos-wrapper-for-nix.patch', 'adapt-kokkos-wrapper-for-hpx.patch'])
+    #depends_on(kokkos_string + ' -cuda -cuda_lambda -wrapper',when='+kokkos -cuda', patches=['adapt-kokkos-wrapper-for-nix.patch', 'adapt-kokkos-wrapper-for-hpx.patch'] )
+    #for gfx in ROCmPackage.amdgpu_targets:                                                                                                                                                                                                                    
+    #    depends_on(kokkos_string + ' +cuda +cuda_lambda +wrapper amdgpu_target={0}'.format(gfx), when='+kokkos +cuda amdgpu_target={0}'.format(gfx))
+
     def cmake_args(self):
         spec = self.spec
         args = []
@@ -88,15 +96,28 @@ class Octotiger(CMakePackage):
         # CUDA
         args.append(self.define_from_variant('OCTOTIGER_WITH_CUDA', 'cuda'))
         args.append(self.define_from_variant('OCTOTIGER_WITH_KOKKOS', 'kokkos'))
+        if '+cuda' in spec:
+            cuda_arch_list = spec.variants['cuda_arch'].value
+            cuda_arch = cuda_arch_list[0]
+            if cuda_arch != 'none':
+                args.append('-DOCTOTIGER_CUDA_ARCH=sm_{0}'.format(cuda_arch))
 
-        args.append(self.define('OCTOTIGER_KOKKOS_SIMD_LIBRARY', 'KOKKOS'))
-        args.append(self.define('OCTOTIGER_KOKKOS_SIMD_EXTENSION', 'SCALAR'))
-        args.append(self.define('OCTOTIGER_WITH_MONOPOLE_HOST_HPX_EXECUTOR', 'OFF'))
-        args.append(self.define('OCTOTIGER_WITH_MULTIPOLE_HOST_HPX_EXECUTOR', 'OFF'))
-        args.append(self.define('OCTOTIGER_WITH_HYDRO_HOST_HPX_EXECUTOR', 'OFF'))
-        args.append(self.define('OCTOTIGER_WITH_KOKKOS_MULTIPOLE_TASKS', '1'))
-        args.append(self.define('OCTOTIGER_WITH_KOKKOS_MONOPOLE_TASKS', '1'))
-        args.append(self.define('OCTOTIGER_WITH_KOKKOS_HYDRO_TASKS', '1'))
+        args.append(self.define_from_variant('OCTOTIGER_KOKKOS_SIMD_LIBRARY', 'simd_library'))
+        args.append(self.define_from_variant('OCTOTIGER_KOKKOS_SIMD_EXTENSION', 'simd_extension'))
+        args.append(self.define_from_variant('OCTOTIGER_WITH_MONOPOLE_HOST_HPX_EXECUTOR', 'kokkos_hpx_kernels'))
+        args.append(self.define_from_variant('OCTOTIGER_WITH_MULTIPOLE_HOST_HPX_EXECUTOR', 'kokkos_hpx_kernels'))
+        args.append(self.define_from_variant('OCTOTIGER_WITH_HYDRO_HOST_HPX_EXECUTOR', 'kokkos_hpx_kernels'))
+        args.append(self.define_from_variant('OCTOTIGER_WITH_KOKKOS_MULTIPOLE_TASKS', 'multipole_host_tasks'))
+        args.append(self.define_from_variant('OCTOTIGER_WITH_KOKKOS_MONOPOLE_TASKS', 'monopole_host_tasks'))
+        args.append(self.define_from_variant('OCTOTIGER_WITH_KOKKOS_HYDRO_TASKS', 'hydro_host_tasks'))
+
+        if "~kokkos_hpx_kernels" in spec or "-kokkos_hpx_kernels" in spec:
+            if spec.variants["multipole_host_tasks"].value > 1:
+                raise SpackError("multipole_host_tasks > 1 requires +kokkos_hpx_kernels")
+            if spec.variants["monopole_host_tasks"].value > 1:
+                raise SpackError("monopole_host_tasks > 1 requires +kokkos_hpx_kernels")
+            if spec.variants["hydro_host_tasks"].value > 1:
+                raise SpackError("hydro_host_tasks > 1 requires +kokkos_hpx_kernels")
 
         # test
         args.append(self.define('OCTOTIGER_WITH_TESTS', self.run_tests))
