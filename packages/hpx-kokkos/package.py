bde9feba50da4b8a24438fbc563d8e265bacfb4a
Gregor Daiss <Gregor.Daiss+git@gmail.com>
2023-08-25 15:46:50 -0500

    Upgrade hpx-kokkos to 0.4.0 candidate

--- packages/hpx-kokkos/package.py
+++ packages/hpx-kokkos/package.py
@@ -11,9 +11,11 @@ class HpxKokkos(CMakePackage, CudaPackage, ROCmPackage):
 
     homepage = "https://github.com/STEllAR-GROUP/hpx-kokkos"
     url = "https://github.com/STEllAR-GROUP/hpx-kokkos/archive/0.0.0.tar.gz"
+    git = "https://github.com/STEllAR-GROUP/hpx-kokkos.git"
     maintainers("G-071", "msimberg")
 
     version("master", git="https://github.com/STEllAR-GROUP/hpx-kokkos.git", branch="master")
+     version("0.4.0", branch="release-0.4.X")
     version("0.3.0", sha256="83c1d11dab95552ad0abdae767c71f757811d7b51d82bd231653dc942e89a45d")
     version("0.2.0", sha256="289b711cea26afe80be002fc521234c9194cd0e8f69863f3b08b654674dbe5d5")
     version("0.1.0", sha256="24edb817d0969f4aea1b68eab4984c2ea9a58f4760a9b8395e20f85b178f0850")
@@ -90,7 +92,6 @@ class HpxKokkos(CMakePackage, CudaPackage, ROCmPackage):
     def check(self):
         if self.run_tests:
             with working_dir(self.build_directory):
-                # These targets are not build normally, so we just build them now
-                make("tests")
-                make("benchmarks")
+                cmake("--build", ".", "--target", "tests")
+                cmake("--build", ".", "--target", "benchmarks")
                 ctest("--output-on-failure")
